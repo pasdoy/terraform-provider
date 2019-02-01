2ba6a46da67ccda152847fc4ffc69d9105075ea7
pasdoy <pasdoy12@gmail.com>
2019-02-01 00:51:45 -0800

    update example

--- example.tf
+++ example.tf
@@ -2,9 +2,16 @@ provider "cloudkarafka" {}
 
 resource "cloudkarafka_instance" "kafka_bat" {
   name   = "terraform-provider-test"
-  plan   = "bat-3"
+  plan   = "ducky"
   region = "amazon-web-services::us-east-1"
-  vpc_subnet = "10.201.0.0/24"
+  //vpc_subnet = "10.201.0.0/24"
+}
+
+resource "cloudkarafka_instance_alarm" "test" {
+  type = "cpu"
+  apikey = "${cloudkarafka_instance.kafka_bat.apikey}"
+  value_threshold = 85
+  time_threshold = 300
 }
 
 output "kafka_brokers" {
