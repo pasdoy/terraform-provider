11a22aaee6b4f20f67fb5480c684df5dd801ab17
pasdoy <pasdoy12@gmail.com>
2019-02-01 00:52:53 -0800

    make work instances

--- resource_instance.go
+++ resource_instance.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/84codes/go-api/api"
+	"github.com/dghubble/sling"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -11,6 +11,9 @@ func resourceInstance() *schema.Resource {
 		Read:   resourceRead,
 		Update: resourceUpdate,
 		Delete: resourceDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
 		Schema: map[string]*schema.Schema{
 			"name": {
 				Type:        schema.TypeString,
@@ -22,9 +25,18 @@ func resourceInstance() *schema.Resource {
 				Required:    true,
 				Description: "Name of the plan",
 			},
+			"tags": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "Tags",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
 			"region": {
 				Type:        schema.TypeString,
 				Required:    true,
+				ForceNew:    true,
 				Description: "Name of the region you want to create your instance in",
 			},
 			"vpc_subnet": {
@@ -58,14 +70,15 @@ func resourceInstance() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Sensitive:   true,
-				Description: "API key for the CloudAMQP instance",
+				Description: "API key for the Kafka cluster",
 			},
 		},
 	}
 }
 
 func resourceCreate(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
+	api := meta.(*sling.Sling)
+
 	keys := []string{"name", "plan", "region", "vpc_subnet"}
 	params := make(map[string]interface{})
 	for _, k := range keys {
@@ -73,42 +86,54 @@ func resourceCreate(d *schema.ResourceData, meta interface{}) error {
 			params[k] = v
 		}
 	}
-	data, err := api.Create(params)
+
+	data := make(map[string]interface{})
+	_, err := api.Post("/api/instances").BodyJSON(params).ReceiveSuccess(&data)
 	if err != nil {
 		return err
 	}
-	d.SetId(data["id"].(string))
+
+	d.SetId(idToString(data["id"]))
 	for k, v := range data {
 		d.Set(k, v)
 	}
+
 	return nil
 
 }
 
 func resourceRead(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
-	data, err := api.Read(d.Id())
+	api := meta.(*sling.Sling)
+
+	data := make(map[string]interface{})
+	_, err := api.Get("/api/instances/" + d.Id()).ReceiveSuccess(&data)
 	if err != nil {
 		return err
 	}
+
 	for k, v := range data {
 		d.Set(k, v)
 	}
+
 	return nil
 
 }
 
 func resourceUpdate(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
+	api := meta.(*sling.Sling)
+
 	keys := []string{"name", "plan"}
 	params := make(map[string]interface{})
 	for _, k := range keys {
 		params[k] = d.Get(k)
 	}
-	return api.Update(d.Id(), params)
+
+	_, err := api.Put("/api/instances/" + d.Id()).BodyJSON(params).ReceiveSuccess(nil)
+	return err
 }
 
 func resourceDelete(d *schema.ResourceData, meta interface{}) error {
-	api := meta.(*api.API)
-	return api.Delete(d.Id())
+	api := meta.(*sling.Sling)
+	_, err := api.Delete("/api/instances/" + d.Id()).ReceiveSuccess(nil)
+	return err
 }
